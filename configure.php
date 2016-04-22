06c4af8eb9b149531c1303176e7a2b4eb7e617a1
richardscottchance <richardscottchance@yahoo.com>
2016-04-22 10:43:23 +0800

    no message

---
+++ configure.php
@@ -0,0 +1,12 @@
+<?php
+ 
+// Database config variables
+define("DB_HOST", "localhost");
+define("DB_USER", "host");
+define("DB_PASSWORD", "g0iktpawsPW");
+define("DB_DATABASE", "pilot_gcm");
+ 
+// Google Cloud Messaging API Key
+// Place your Google API Key
+define("GOOGLE_API_KEY", "AIzaSyCwj0coIrKvdtfSfLWPbgutp8771GGzqfs");                           
+?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+require_once('loader.php');
+ 
+// return json response 
+$json = array();
+ 
+
+ 
+// GCM Registration ID got from device
+$gcmRegID  = $_REQUEST["regId"];
+ 
+ 
+/**
+ * Registering a user device in database
+ * Store reg id in users table
+ */
+if (isset($gcmRegID)) {
+     
+    // Store user details in db
+    $res = storeUser($gcmRegID);
+}
+
+$category = $_REQUEST["category"];
+$title    = $_REQUEST["message"];
+$registatoin_ids = getAllUsers();
+
+if(count($registatoin_ids) && isset($category) && isset($title)) {
+    $message = array
+   (
+    'message'  => $title,
+    'title'  => 'I Know the Pilot',
+    'subtitle' => $category,
+    'tickerText' => 'I Know the Pilot',
+    'vibrate' => 1,
+    'sound'  => 1,
+    'largeIcon' => 'large_icon',
+    'smallIcon' => 'small_icon'
+   );
+   
+ 
+    $result = send_push_notification($registatoin_ids, $message);
+ 
+    echo $result;
+}
+?>
