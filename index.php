05e8df28ed0accf491f1cf283c285fad7b40be42
liming <liming8964@hotmail.com>
2016-05-19 05:44:44 +0800

    no message

--- index.php
+++ index.php
@@ -33,8 +33,6 @@ if (isset($gcmRegID) && isset($my_categories)) {
 
 
 $category = $_REQUEST["category"];
-echo $category;
-
 $title    = $_REQUEST["message"];
 
 if(count($registatoin_ids) && isset($category) && isset($title)) {
