53b278ee92c3200d79b1adfdc124efb1e69d3ccc
Liming Jin <liming8964@hotmail.com>
2018-05-29 07:31:07 +0800

    Update configure.php

--- configure.php
+++ configure.php
@@ -11,3 +11,14 @@ define("DB_DATABASE", "pilot_gcm");
 define("GOOGLE_API_KEY", "AIzaSyCwj0coIrKvdtfSfLWPbgutp8771GGzqfs");
 define("GOOGLE_API_KEY_IOS", "AIzaSyDbjy7wgPySMbVDkcjEBck_ouKoea0p8bY");                        
 ?>
+
+EOOADRROHMFAHCMVGVC3RQS
+
+ECA7EUJVVVVEXT0TVWNEQFM
+
+Email: boris.ivanov815@yandex.com
+Password: Xw6103pkytUO
+
+username : s359050
+password : eDjSyFTx3t
+
